52da1f35e2cf0e137c563a566bda457215b188cc
chrisfisch914 <chrisfischer@ChristophersMBP.attlocal.net>
2020-12-20 12:44:30 -0600

    Improved makeDay.sh

--- makeDay.sh
+++ makeDay.sh
@@ -6,4 +6,6 @@ touch Day${1}/day${1}-2.py
 touch Day${1}/input.txt
 
 printf "import re\n" >> Day${1}/day${1}-1.py
-printf "import re\n" >> Day${1}/day${1}-2.py
+printf "import sys\nsys.path.append("..")\nfrom input_reader import InputReader\n" >> Day${1}/day${1}-1.py
+
+cat Day${1}/day${1}-1.py >> Day${1}/day${1}-2.py
